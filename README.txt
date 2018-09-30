55a83fd7b89aa002cdc7fb481e58c4e1655da4a1
K4-713 <katie@katiehorn.com>
2018-09-30 15:09:42 -0400

    Read and parse track information from itunes library export xml

---
+++ README.txt
@@ -0,0 +1,16 @@
+
+iTunes library toy. CLI, hackjob. Enjoy in good health.
+
+Structure notes:
+See the attached sample xml
+
+I have just gotten to the point where I read the library and stuff info I 
+personally care about into a php array for easier processing.
+
+Fun stuff I want to do next:
+    * Genre counter
+    * Artist list
+    * Artists I basically hate (no songs over two stars, sorted by song count)
+    * Best and worst skip-to-play ratios (artists, songs, albums)
+	* Hehe playlist generator for the best ratios
+    * Some concept of favorite album... rating+playcount based
---
+++ tuneparse.php
@@ -0,0 +1,134 @@
+<?php
+
+/* 
+ * Copyright (C) 2018 k4
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$results = getAllSongData();
+$songdata = parseAllSongData($results);
+
+$count = sizeof($songdata);
+
+outline("Parsed $count songs");
+
+/**
+ * Load the simpleXML object up with data from the file we want...
+ * @staticvar type $xml
+ * @return loaded simpleXML
+ */
+function getSimpleXML() {
+    static $xml = null;
+
+    if (is_null($xml)) {
+	outline("Loading XML library...");
+	//TODO: Be nice to people who aren't you. Later.
+	$xml = simplexml_load_file('./exported_xml/Library_Backup_20180928.xml');
+	outline("XML library loaded");
+    }
+
+    return $xml;
+}
+
+/**
+ * Do an xpath query, and return the results
+ * @param string $query The xpath query to run
+ * @return xpath query results
+ */
+function doXpath($query) {
+    $xml = getSimpleXML();
+    //TODO: Some error handling in here.
+    return $xml->xpath($query);
+}
+
+function getAllSongIDs() {
+    return doXpath('/plist/dict/dict/key');
+}
+
+function getAllSongData() {
+    $ret = doXpath('/plist/dict/dict/key/following-sibling::dict[1]');
+    outline("Found all the songs.");
+    return $ret;
+}
+
+function parseSingleSong($data) {
+
+    //stuff we currently care about enough to pull
+    $pull_me = array(
+	'Track ID' => 'integer',
+	'Name' => 'string',
+	'Artist' => 'string',
+	'Album' => 'string',
+	'Genre' => 'string',
+	'Year' => 'integer',
+	'Play Count' => 'integer',
+	'Skip Count' => 'integer',
+	'Rating' => 'integer',
+    );
+
+    $return_data = array();
+
+    //This is all stored in a key/value structure in the xml, with the nodes 
+    //adjacent to eachother.
+    //...Dicks.
+    $grab_next = false;
+    foreach ($data as $key => $value) {
+	$value = (string) $value;
+	if ($key === 'key') { //phase 1
+	    $grab_next = false;
+	    if (array_key_exists($value, $pull_me)) {
+		$grab_next = $value;
+	    }
+	} else {    //phase 2
+	    if ($grab_next !== false) {
+		if ($key === $pull_me[$grab_next]) {
+		    //success!
+		    $return_data[$grab_next] = $value;
+		} else {
+		    //complain!
+		    outline("OMG, wrong node type. Expecting $pull_me[$grab_next], got $key");
+		    outline($data);
+		    die();
+		}
+		$grab_next = false;
+	    }
+	}
+    }
+    outline(".", false);
+    return $return_data;
+}
+
+function parseAllSongData($songs) {
+    $ret = array();
+    foreach ($songs as $song_data) {
+	$ret[] = parseSingleSong($song_data);
+    }
+    outline("Finished parsing all song data");
+    return $ret;
+}
+
+function outline($thing, $newline = true) {
+    if ($newline) {
+	$newline = "\n";
+    } else {
+	$newline = '';
+    }
+    if (is_array($thing)) {
+	echo print_r($thing);
+	echo $newline;
+    } else {
+	echo "$thing$newline";
+    }
+}
